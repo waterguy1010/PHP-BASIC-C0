96b772852bd6da5f0c7ace5ecfb38b8204628a27
Abdel Souhali <90617360+waterguy1010@users.noreply.github.com>
2022-01-18 08:39:33 +0100

    verbeterd php 2.10

--- 2-Variabelen-en-datatype/2.10 - String concatenation 2/index.php
+++ 2-Variabelen-en-datatype/2.10 - String concatenation 2/index.php
@@ -1,20 +1,18 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+<html>
 <body>
-    <?php
-    
-    $item1 = "piepi";
-    $item2 = "onix";
-    $item3 = "twitch";
-    
-    echo "<uL><li>$item1</li><li>$item2</li><li>$item3</li></ul>";
-    
-    ?>
+
+<?php
+
+
+$a = "<li>item 1</li>";
+$a .= "<li>item 2</li> ";
+$a .= "<li> item 3 </li>";
+
+echo "<ul> $a </ul>";
+
+
+?>
+
 </body>
 </html>
